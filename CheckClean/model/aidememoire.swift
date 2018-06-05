97a1ee6b2aa12d19c74dc07bf45924bd63978aff
Machado Sergio <mikialey@gmail.com>
2018-06-05 16:49:18 +0200

    add files git

---
+++ CheckClean/model/aidememoire.swift
@@ -0,0 +1,14 @@
+/*
+ if MFMailComposeViewController.canSendMail() {
+ 
+ self.messageController = MFMailComposeViewController()
+ self.messageController.mailComposeDelegate = self
+ messageController.setSubject("Control")
+ messageController.setMessageBody("Un message de test avec pièce jointe", isHTML: false)
+ 
+ messageController.addAttachmentData(pdfData, mimeType: "application/pdf", fileName: "example.pdf")
+ self.present(messageController, animated: true)
+ }
+ else {
+ print("cannot send mail")
+ }*/
